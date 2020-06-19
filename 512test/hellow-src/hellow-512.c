6ca736680013e6ab1a540e9c0c561484065b9a7a
Edward Vidal Jr <develone@sbcglobal.net>
2020-06-19 09:30:42 -0600

    adding hellow.c & chgs needed for mpich test

---
+++ 512test/hellow-src/hellow-512.c
@@ -0,0 +1,25 @@
+/* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil ; -*- */
+/*
+ *  (C) 2001 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
+
+#include <stdio.h>
+#include "mpi.h"
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int rank;
+    int size;
+    int status;
+
+    MPI_Init(0, 0);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    printf("Hello world from process %d of %d\n", rank, size);
+    status = system("pwd");
+    status = system("/home/devel/mpich-3.3.2/examples/master");
+    MPI_Finalize();
+    return 0;
+}
